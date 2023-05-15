cae341402806a864dfdd46250628461e61e9d16f
Thunder <pascensao77@gmail.com>
2023-05-15 15:43:51 +0100

    Meta 2

--- src/FilaURL_I.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.*;
-
-public interface FilaURL_I extends Remote {
-    public String sendUrl() throws RemoteException;
-
-    public boolean recUrl(String url) throws RemoteException;
-
-    public void adicionarAForca(String url) throws RemoteException;
-}
