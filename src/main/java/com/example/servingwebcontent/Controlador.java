1634ed2f52a3599945ec3bbf786d706c0c3d3793
Thunder <pascensao77@gmail.com>
2023-05-17 23:06:35 +0100

    Fase de limpeza

--- src/main/java/com/example/servingwebcontent/Controlador.java
+++ src/main/java/com/example/servingwebcontent/Controlador.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
-public class GreetingController {
+public class Controlador {
 
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
